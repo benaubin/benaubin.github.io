83cb3d2bebeaa4cf692fa73974c80332106c19f9
Ben! <ben@bensites.com>
2015-08-23 14:39:44 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -26,30 +26,38 @@ angular.module('BenSites',[])
         $scope.projects = [
                 {
                     name: 'Room305',
-                    desc: "Room305 is an awesome student literary magizine. " +
-                          "I've helped with most of the backend management and design.",
+                    desc: "Room305 is an awesome student literary magizine." +
+                          "I've helped with most of the backend management, hosting, and design.",
                     size: 3,
                     url: 'Room305.org'
                 },
                 {
                     name: 'PlanHW',
                     desc: "PlanHW is your new homework planner. It has features that understand you, not make you understand them. " +
-                    "Easily add homework, and get reminders at a great time to work on it. Your teachers can easily send you homework, "+
-                    "so you don't have to do anything at all. Did I mention it's free?",
+                    "Easily add homework using plain english, pretty much as you'd describe it to any other human, and get reminders " +
+                    "at a great time to work on it, instead of when it's due. Oh - and your teachers can send you homework just as easily, "+
+                    "so you don't have to type anything at all. Did I mention it's free?",
                     size: 6,
                     url: 'PlanHW.com'
+                },
+                {
+                    name: 'St. Bernard Ski & Travel Club',
+                    desc: "Working closely with a ski club, I developed a Wordpress website as it was requested, and " + 
+                          "helped them understand and use their website, and its backend. Working through Skype, I was able to " +
+                          "perfect each and every aspect of their design.",
+                    size: 3,
+                    url: 'Stbernardski.org'
                 },
                 {
                     name: 'EasyCalc',
                     desc: "EasyCalc knows 1 + 1 is. It knows what you mean when you are typing, and it's modular, " + 
                     "meaning you can write your own operators. It's even open source, so everyone has the best calculator. Ever.",
-                    size: 3,
+                    size: 2,
                     url: 'EasyCalc.bensites.com'
                 },
                 {
                     name: 'AllForFun\'s Modpack',
-                    size: '2',
-                    class: 'col-md-offset-1',
+                    size: 2,
                     url: 'www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/mod-packs/2146221-allforfuns-modpack-its-a-custom-coded-modpack-in-a',
                     desc: 'A custom coded modpack for Minecraft in a mod, this was one of my first projects, and it\'s pretty cool. Although it\'s not currently in active development, at it\'s peek, over 6 developers contributed with over 150 players downloading it in a month.'
                 },
